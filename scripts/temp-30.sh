bed43bb387d4f4b783013136f28f031818e6f389
Jorge Aznar <jorgeatgu@gmail.com>
2019-02-07 22:48:03 +0100

    remove TODO

--- scripts/temp-30.sh
+++ scripts/temp-30.sh
@@ -3,19 +3,14 @@
 <<comentario
 Dependencias: jq - sed(linux)
 A partir de los JSON en bruto vamos a obtener un CSV.
-Este CSV solo va a contener aquella temperaturas mínimás iguales o superiores a 20ºC.
+Este CSV solo va a contener aquella temperaturas máximas iguales o superiores a 30ºC.
 Esto es lo que se llama una noche tropical.
-Ahora vamos a seleccionar aquellos valores que son iguales o superiores a 20ºC en .tmin
-Y nos quedamos con los valores de .fecha y .tmin
+Ahora vamos a seleccionar aquellos valores que son iguales o superiores a 30ºC en .tmax
+Y nos quedamos con los valores de .fecha y .tmax
 Al final pipeamos con la opcion @csv para que exporte un CSV.
 El archivo generado se sigue quedando con algunas comillas dobles.
 No hacen nada malo pero a mí personalmente me MOLESTAN(TOC).
 Así que las eliminamos con sed.
-
-To-Do:
-
-Array con todos los indicativos de las estaciones.
-Iterar sobre el array con un for para obtener un CSV de cada estación.
 comentario
 
 # Generamos el array cargando la lista de nombres de estación
@@ -29,7 +24,7 @@ readarray -t year < ~/github/data/year.csv
 
 # Recorremos el array de nombre de estación
 for (( i=0; i<${#nombre[@]}; ++i )); do
-    # Obtenemos un CSV solamente con la fecha, y temperaturas máximas iguales o superiores a 20ºC
+    # Obtenemos un CSV solamente con la fecha, y temperaturas máximas iguales o superiores a 30ºC
     jq -r '["fecha", "maxima"], (.[] | select(.tmax >= 30) | select(.tmax != null) | [.fecha, .tmax]) | @csv' ~/github/data/diarias/${indicativo[$i]}-total-diario.json > ~/github/data/temp-30/${nombre[$i]}-temp-30.csv
     echo "${nombre[$i]} terminada!"
 done
