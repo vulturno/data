af7a94a8517caf2aceabc319ed09e3177691a6a5
Jorge Aznar <jorgeatgu@gmail.com>
2019-02-26 21:52:56 +0100

    new script :tropical_fish:

---
+++ scripts/count-min-day-by-day.sh
@@ -0,0 +1,39 @@
+#!/usr/local/bin/bash
+
+: '
+Dependencias: csvkit - sed(linux) - bash 4.0
+
+Importante, para ejecutar este script se necesita
+haber lanzado: temperature-max-day-by-day.sh
+
+Entro en el for, corresponde a los años.
+Estamos en 1950 y acto seguido entro al siguiente for, corresponde a estaciones.
+Seguimos en 1950 pero en la estación de la AEMET de Albacete.
+Ahora con CSVGREP busco en la columna fecha todas las fechas
+que comiencen por 1950 y las guardo en un archivo de records.
+Así hasta 2018 y con todas las estaciones.
+En último lugar elimino todas las cabeceras de los CSV(fecha, máxima) de todos los archivos
+que he generado.
+
+Tiempo aproximado de ejecución: 30 minutos
+'
+
+# Generamos el array cargando la lista de nombres de estación
+readarray -t nombre < ~/github/data/stations-name.csv
+
+# Generamos el array cargando la lista de los indicativos de cada estación
+readarray -t year < ~/github/data/year.csv
+
+# Recorremos el array de años
+for ((i = 0; i < ${#year[@]}; i++)); do
+
+    for ((j = 0; j < ${#nombre[@]}; j++)); do
+
+        csvgrep -c fecha -r "^${year[$i]}" ~/github/data/records-dias/minimas/"${nombre[$j]}"-records.csv >> ~/github/data/records-dias/minimas/records-minima-year.csv
+
+    done
+    echo "${year[$i]} terminada!"
+
+done
+
+sed -i '2,${/fecha/d;}' ~/github/data/records-dias/minimas/records-minima-year.csv
---
+++ scripts/temperature-min-day-by-day.sh
@@ -0,0 +1,44 @@
+#!/usr/local/bin/bash
+
+: '
+Dependencias: csvkit - sed(linux) - bash 4.0 >
+A partir de todos los CSV de todas las estaciones
+Vamos a obtener el record de temperatura máxima
+de cada dia del año entre la serie de años de
+cada estación.
+Primero recorro cada estación con un for, en cuanto este en la primera estación
+entro en el siguiente for que recorrerá los 366 días del año.
+Ahora con csvgrep busco un día en el archivo de la estación.
+Por ejemplo: en el primer bucle obtengo todos los uno de enero de TODOS los años.
+Ahora ordeno los resultados en base a la columna de temperatura máxima,
+así el primer resultado sera la temperatura más alta de ese día.
+Lo guardamos en temp.csv
+Ahora eliminamos con sed el resto de de días y lo guardamos con el nombre de la estación.
+Así con los 366 días del año y con las 45 estaciones que analizo.
+En último lugar elimino todas las cabeceras de los CSV(fecha, máxima) de todos los archivos
+que he generado.
+
+Tiempo aproximado de ejecución: 4 horas
+'
+
+# Generamos el array cargando la lista de nombres de estación
+readarray -t nombre < ~/github/data/stations-name.csv
+
+# Generamos el array cargando la lista de los indicativos de cada estación
+readarray -t days < ~/github/data/dias.csv
+
+# Recorremos el array de nombre de estación
+for ((i = 0; i < ${#nombre[@]}; i++)); do
+
+    for ((j = 0; j < ${#days[@]}; j++)); do
+
+        csvgrep -c fecha -r "${days[$j]}$" ~/github/data/day-by-day/"${nombre[$i]}"-diarias.csv | csvsort -c minima > temp.csv &&
+        sed '1,2!d' temp.csv >> ~/github/data/records-dias/minimas/"${nombre[$i]}"-records.csv &&
+        rm temp.csv
+
+    done
+    echo "${nombre[$i]} terminada!"
+
+done
+
+sed -i '2,${/fecha/d;}' ~/github/data/records-dias/minimas/*.csv
