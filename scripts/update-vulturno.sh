993c69cc823a2b4e3f00c2ef56850b4d9d9be9dd
Jorge Aznar <jorgeatgu@gmail.com>
2019-10-07 08:42:58 +0200

    update September

--- scripts/update-vulturno.sh
+++ scripts/update-vulturno.sh
@@ -7,10 +7,10 @@ bash day-by-day.sh &&
 bash frosty.sh &&
 bash tropical.sh &&
 bash tropical-cities.sh &&
-bash temperature-max-month.sh "$month" &&
-bash temperature-min-month.sh "$month" &&
-bash temperature-last-two-records-max-month.sh "$month" &&
-bash temperature-last-two-records-min-month.sh "$month" &&
+bash temperature-max-month.sh septiembre 09 &&
+bash temperature-min-month.sh septiembre 09 &&
+bash temperature-last-two-records-max-month.sh Septiembre &&
+bash temperature-last-two-records-min-month.sh Septiembre &&
 bash temperature-max-day-by-day-count-month.sh &&
 bash temperature-min-day-by-day-count-month.sh &&
 bash temp-extreme.sh &&
