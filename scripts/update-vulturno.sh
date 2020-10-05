e85bbb10374687604fcd86da7bf2ea3f03d7dd26
Jorge <jorgeatgu@gmail.com>
2020-10-05 22:49:13 +0200

    update months

--- scripts/update-vulturno.sh
+++ scripts/update-vulturno.sh
@@ -8,10 +8,10 @@ bash tropical-cities.sh &&
 bash temp-extreme.sh &&
 bash day-by-day.sh &&
 bash frosty.sh &&
-bash temperature-max-month.sh julio 07 &&
-bash temperature-min-month.sh julio 07 &&
-bash temperature-last-two-records-max-month.sh Julio &&
-bash temperature-last-two-records-min-month.sh Julio &&
+bash temperature-max-month.sh septiembre 09 &&
+bash temperature-min-month.sh septiembre 09 &&
+bash temperature-last-two-records-max-month.sh Septiembre &&
+bash temperature-last-two-records-min-month.sh Septiembre &&
 bash temperature-max-day-by-day-count-month.sh &&
 bash temperature-min-day-by-day-count-month.sh &&
 bash count-records.sh
