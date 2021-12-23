80525fed63700272ea359919d8df9ff558081218
Jorge <jorgeatgu@gmail.com>
2021-12-23 19:12:31 +0100

    update data

--- scripts/update-vulturno.sh
+++ scripts/update-vulturno.sh
@@ -5,13 +5,10 @@ month=$1
 
 bash day-by-day.sh &&
 bash frosty.sh &&
-bash tropical.sh &&
-bash tropical-cities.sh &&
-bash temp-extreme.sh &&
-bash temperature-max-month.sh septiembre 09 &&
-bash temperature-min-month.sh septiembre 09 &&
-bash temperature-last-two-records-max-month.sh Septiembre &&
-bash temperature-last-two-records-min-month.sh Septiembre &&
+bash temperature-max-month.sh noviembre 11 &&
+bash temperature-min-month.sh noviembre 11 &&
+bash temperature-last-two-records-max-month.sh Noviembre &&
+bash temperature-last-two-records-min-month.sh Noviembre &&
 bash temperature-max-day-by-day-count-month.sh &&
 bash temperature-min-day-by-day-count-month.sh &&
 bash count-records.sh
