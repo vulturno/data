b92aae9555bb3c43419585fae646c060743757e2
Jorge <jorgeatgu@gmail.com>
2022-01-16 18:51:21 +0100

    update: december

--- scripts/update-vulturno.sh
+++ scripts/update-vulturno.sh
@@ -5,10 +5,10 @@ month=$1
 
 bash day-by-day.sh &&
 bash frosty.sh &&
-bash temperature-max-month.sh noviembre 11 &&
-bash temperature-min-month.sh noviembre 11 &&
-bash temperature-last-two-records-max-month.sh Noviembre &&
-bash temperature-last-two-records-min-month.sh Noviembre &&
+bash temperature-max-month.sh diciembre 12 &&
+bash temperature-min-month.sh diciembre 12 &&
+bash temperature-last-two-records-max-month.sh Diciembre &&
+bash temperature-last-two-records-min-month.sh Diciembre &&
 bash temperature-max-day-by-day-count-month.sh &&
 bash temperature-min-day-by-day-count-month.sh &&
 bash count-records.sh
