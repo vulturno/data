c09ecbfedb34e3986db12bf0a32442dbc9f77e79
Jorge <jorgeatgu@gmail.com>
2022-06-12 12:42:33 +0200

    update may

--- scripts/update-vulturno.sh
+++ scripts/update-vulturno.sh
@@ -5,10 +5,13 @@ month=$1
 
 bash day-by-day.sh &&
 bash frosty.sh &&
-bash temperature-max-month.sh enero 01 &&
-bash temperature-min-month.sh enero 01 &&
-bash temperature-last-two-records-max-month.sh Enero &&
-bash temperature-last-two-records-min-month.sh Enero &&
+bash temp-extreme.sh &&
+bash tropical.sh &&
+bash tropical-cities.sh &&
+bash temperature-max-month.sh mayo 05 &&
+bash temperature-min-month.sh mayo 05 &&
+bash temperature-last-two-records-max-month.sh Mayo &&
+bash temperature-last-two-records-min-month.sh Mayo &&
 bash temperature-max-day-by-day-count-month.sh &&
 bash temperature-min-day-by-day-count-month.sh &&
 bash count-records.sh
