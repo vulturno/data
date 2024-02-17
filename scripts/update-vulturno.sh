55fdfe6217370a99c7b92cd56a1aadda288aecee
jorgeatgu <jorgeatgu@gmail.com>
2024-02-17 11:25:44 +0100

    update january

--- scripts/update-vulturno.sh
+++ scripts/update-vulturno.sh
@@ -9,10 +9,10 @@ month=$1
 bash day-by-day.sh &&
 bash frosty.sh &&
 bash frost-cities.sh &&
-bash temperature-max-month.sh diciembre 12 &&
-bash temperature-min-month.sh diciembre 12 &&
-bash temperature-last-two-records-max-month.sh Diciembre &&
-bash temperature-last-two-records-min-month.sh Diciembre &&
+bash temperature-max-month.sh enero 01 &&
+bash temperature-min-month.sh enero 01 &&
+bash temperature-last-two-records-max-month.sh Enero &&
+bash temperature-last-two-records-min-month.sh Enero &&
 bash temperature-max-day-by-day-count-month.sh &&
 bash temperature-min-day-by-day-count-month.sh &&
 bash count-records.sh
