3408c04767202067f2cdbdbcca1f960a04233426
jorgeatgu <jorgeatgu@gmail.com>
2024-07-05 08:50:06 +0200

    update june

--- scripts/update-vulturno.sh
+++ scripts/update-vulturno.sh
@@ -3,16 +3,16 @@
 vulturno=~/github/vulturno/csv/
 month=$1
 
-# bash temp-extreme.sh &&
-# bash tropical-cities.sh &&
 bash day-by-day.sh &&
 bash tropical.sh &&
+bash tropical-cities.sh &&
+bash temp-extreme.sh &&
 bash frosty.sh &&
 bash frost-cities.sh &&
-bash temperature-max-month.sh mayo 05 &&
-bash temperature-min-month.sh mayo 05 &&
-bash temperature-last-two-records-max-month.sh Mayo &&
-bash temperature-last-two-records-min-month.sh Mayo &&
+bash temperature-max-month.sh junio 06 &&
+bash temperature-min-month.sh junio 06 &&
+bash temperature-last-two-records-max-month.sh Junio &&
+bash temperature-last-two-records-min-month.sh Junio &&
 bash temperature-max-day-by-day-count-month.sh &&
 bash temperature-min-day-by-day-count-month.sh &&
 bash count-records.sh
