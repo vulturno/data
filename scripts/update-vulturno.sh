abeaa41b8ef93ad8615ddb2a065d883079bc0602
jorgeatgu <jorgeatgu@gmail.com>
2024-08-07 17:31:09 +0200

    update july

--- scripts/update-vulturno.sh
+++ scripts/update-vulturno.sh
@@ -7,12 +7,12 @@ bash day-by-day.sh &&
 bash tropical.sh &&
 bash tropical-cities.sh &&
 bash temp-extreme.sh &&
-bash frosty.sh &&
-bash frost-cities.sh &&
-bash temperature-max-month.sh junio 06 &&
-bash temperature-min-month.sh junio 06 &&
-bash temperature-last-two-records-max-month.sh Junio &&
-bash temperature-last-two-records-min-month.sh Junio &&
+bash torridas.sh &&
+bash torridas-cities.sh &&
+bash temperature-max-month.sh julio 07 &&
+bash temperature-min-month.sh julio 07 &&
+bash temperature-last-two-records-max-month.sh Julio &&
+bash temperature-last-two-records-min-month.sh Julio &&
 bash temperature-max-day-by-day-count-month.sh &&
 bash temperature-min-day-by-day-count-month.sh &&
 bash count-records.sh
